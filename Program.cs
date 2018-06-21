6700eb538d572dd79a44c0bccf815896857c8147
Pete K <krautscheid@mitre.org>
2018-06-21 14:43:32 -0400

    Create basic class to wrap a death record and extract fields

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
@@ -25,48 +25,21 @@ namespace csharp_fhir_death_record
             {
                 Console.WriteLine($"Reading file '{path}'");
                 string json = File.ReadAllText(path);
-                FhirJsonParser parser = new FhirJsonParser();
-                Bundle bundle = parser.Parse<Bundle>(json);
-                // TODO: Need to gracefully handle edge conditins like no decedent present
-                Patient decedent = (Patient)bundle.Entry.Find(e => e.Resource.ResourceType.ToString() == "Patient").Resource;
-                Console.WriteLine($"  Decedent Name: {decedent.Name[0].ToString()}");
-                var deceasedDateTime = DateTime.Parse(decedent.Deceased.ToString());
-                Console.WriteLine($"  Time and Date of Death: {String.Format("{0:MMMM d, yyyy \\a\\t h:mmtt}", deceasedDateTime)}");
-                Practitioner certifier = (Practitioner)bundle.Entry.Find(e => e.Resource.ResourceType.ToString() == "Practitioner").Resource;
-                Console.WriteLine($"  Certifier Name: {certifier.Name[0].ToString()}");
-
-                var conditions = bundle.Entry.FindAll(e => e.Resource.ResourceType.ToString() == "Condition").Select(e => (Condition)e.Resource);
-                Regex htmlRegex = new Regex("<.*?>");
-                foreach (var condition in conditions)
-                {
-                    if (condition.Onset != null)
-                    {
-                        Console.WriteLine($"  Cause of Death: {htmlRegex.Replace(condition.Text.Div, "")} ({condition.Onset})");
-                    }
-                    else
-                    {
-                        Console.WriteLine($"  Contributing Conditions: {htmlRegex.Replace(condition.Text.Div, "")}");
-                    }
-                }
-
-                // TODO: The use of FHIR paths seems potentially valuable, but the paths below don't appear to work as expected
-                //PocoNavigator navigator = new PocoNavigator(bundle);
-                //var matches = navigator.Select("Bundle.entry.resource.where(resourceType='Patient').name.family");
-                //var matches = navigator.Select("Bundle.entry.resource.where(meta.profile='http://nightingaleproject.github.io/fhirDeathRecord/StructureDefinition/sdr-decedent-Decedent')");
-                //var matches = navigator.Select("Bundle.entry.resource");
-                //Console.WriteLine($"Found {matches.Count()} matches");
-                //foreach (var match in matches)
-                //{
-                //    Console.WriteLine(match.Value);
-                //    if (match.Type == "Patient")
-                //    {
-                //        var names = match.Select("name.family");
-                //        foreach (var name in names)
-                //        {
-                //            Console.WriteLine($"Name: {name.Value}");
-                //        }
-                //    }
-                //}
+                DeathRecord deathRecord = new DeathRecord(json);
+                Console.WriteLine($"  Given Name: {deathRecord.GivenName}");
+                Console.WriteLine($"  Last Name: {deathRecord.LastName}");
+                Console.WriteLine($"  SSN: {deathRecord.SSN}");
+                Console.WriteLine($"  Cause of Death 1: {deathRecord.COD(0)}");
+                Console.WriteLine($"  Cause of Death Interval 1: {deathRecord.CODInterval(0)}");
+                Console.WriteLine($"  Cause of Death 2: {deathRecord.COD(1)}");
+                Console.WriteLine($"  Cause of Death Interval 2: {deathRecord.CODInterval(1)}");
+                Console.WriteLine($"  Cause of Death 3: {deathRecord.COD(2)}");
+                Console.WriteLine($"  Cause of Death Interval 3: {deathRecord.CODInterval(2)}");
+                Console.WriteLine($"  Cause of Death 4: {deathRecord.COD(3)}");
+                Console.WriteLine($"  Cause of Death Interval 4: {deathRecord.CODInterval(3)}");
+                Console.WriteLine($"  Contributing Conditions: {deathRecord.ContributingConditions}");
+                Console.WriteLine($"  Certifier Given Name: {deathRecord.CertifierGivenName}");
+                Console.WriteLine($"  Certifier Last Name: {deathRecord.CertifierLastName}");
             }
             else
             {

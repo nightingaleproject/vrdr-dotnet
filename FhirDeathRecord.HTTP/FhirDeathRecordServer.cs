0299204aff2623fb2523aac41bb1f33a4d7e91f0
Andrew Hubley <ahubley@mitre.org>
2018-10-25 20:12:55 -0400

    initial rough cut of http server implementation

---
+++ FhirDeathRecord.HTTP/FhirDeathRecordServer.cs
@@ -0,0 +1,75 @@
+using System;
+using System.Net;
+using System.Threading;
+using System.Linq;
+using System.Text;
+
+namespace FhirDeathRecord.HTTP
+{
+    public class FhirDeathRecordServer
+    {
+        private readonly HttpListener _listener = new HttpListener();
+        private readonly Func<HttpListenerRequest, string> _responderMethod;
+
+        public FhirDeathRecordServer(string[] prefixes, Func<HttpListenerRequest, string> method)
+        {
+            // URI prefixes are required, for example 
+            // "http://localhost:8080/index/".
+            if (prefixes == null || prefixes.Length == 0)
+                throw new ArgumentException("prefixes");
+ 
+            // A responder method is required
+            if (method == null)
+                throw new ArgumentException("method");
+ 
+            foreach (string s in prefixes)
+                _listener.Prefixes.Add(s);
+ 
+            _responderMethod = method;
+            _listener.Start();
+        }
+
+         public FhirDeathRecordServer(Func<HttpListenerRequest, string> method, params string[] prefixes)
+            : this(prefixes, method) { }
+
+        public void Run()
+        {
+            ThreadPool.QueueUserWorkItem((o) =>
+            {
+                Console.WriteLine("Webserver running...");
+                try
+                {
+                    while (_listener.IsListening)
+                    {
+                        ThreadPool.QueueUserWorkItem((c) =>
+                        {
+                            var ctx = c as HttpListenerContext;
+                            try
+                            {
+                                string rstr = _responderMethod(ctx.Request);
+                                byte[] buf = Encoding.UTF8.GetBytes(rstr);
+                                ctx.Response.ContentLength64 = buf.Length;
+                                ctx.Response.OutputStream.Write(buf, 0, buf.Length);
+                            }
+                            catch { } // suppress any exceptions for now
+                            finally
+                            {
+                                // always close the stream
+                                ctx.Response.OutputStream.Close();
+                            }
+                        }, _listener.GetContext());
+                    }
+                }
+                catch { } // suppress any exceptions
+            });
+        }
+ 
+        public void Stop()
+        {
+            _listener.Stop();
+            _listener.Close();
+        }
+    }
+ 
+    }
+}
---
+++ FhirDeathRecord.HTTP/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Net;
+using System.Threading;
+using System.Linq;
+using System.Text;
+
+namespace FhirDeathRecord.HTTP
+{
+    class Program
+    {
+    static void Main(string[] args)
+    {
+        FhirDeathRecordServer fhirDeathRecordServer = new FhirDeathRecordServer(SendIJEResponse, "http://localhost:8080/ije/");
+        fhirDeathRecordServer.Run();
+        Console.WriteLine("Example FHIR webserver. Press a key to quit.");
+        Console.ReadKey();
+        fhirDeathRecordServer.Stop();
+    }
+
+ 
+    public static string SendIJEResponse(HttpListenerRequest request)
+    {
+        return string.Format("IJE Response Here", DateTime.Now);    
+    }
+    
+  
+ 
+    public static string SendFHIRResponse(HttpListenerRequest request)
+    {
+        return string.Format("FHIR Response Here", DateTime.Now);    
+    }
+}

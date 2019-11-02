650857afb086bac26ed7b5dbf7c72e61b1389149
Yunwei Wang <yunwwang@gmail.com>
2019-11-02 13:10:18 -0400

    create class

---
+++ FhirDeathRecord/InterestedParty.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using Hl7.Fhir.Introspection;
+using Hl7.Fhir.Model;
+
+namespace FhirDeathRecord
+{
+    public class InterestedParty : Organization
+    {}
+}

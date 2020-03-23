c316aa6427fd54ebbba210409bf56235aa9feb40
Marc Hadley <mhadley@mitre.org>
2020-03-23 18:47:55 -0400

    Fix CLI to use modified DeathRecordSubmission constructor

--- VRDR.CLI/Program.cs
+++ VRDR.CLI/Program.cs
@@ -628,10 +628,9 @@ namespace VRDR.CLI
 
                 for (int i = 0; i < 1000; i++)
                 {
-                    DeathRecordSubmission submission = new DeathRecordSubmission();
+                    DeathRecordSubmission submission = new DeathRecordSubmission(record);
                     submission.MessageTimestamp = DateTime.Now;
                     submission.MessageId = "adam-" + Convert.ToString(i) + "-" + Guid.NewGuid();
-                    submission.MessagePayload = record.GetBundle();
                     StringContent stringContent = new StringContent(submission.ToJSON(), Encoding.UTF8, "application/fhir+json");
                     CallEndpoint(args[2], stringContent);
                     Console.WriteLine("Sending " + Convert.ToString(i));

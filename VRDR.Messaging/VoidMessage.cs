e6170da792834656a5989a8087ef5d94ba3cb768
Marc Hadley <mhadley@mitre.org>
2020-05-11 15:57:21 -0400

    Move parameters to BaseMessage

--- VRDR.Messaging/VoidMessage.cs
+++ VRDR.Messaging/VoidMessage.cs
@@ -6,15 +6,9 @@ namespace VRDR
     /// <summary>Class <c>VoidMessage</c> indicates that a previously submitted DeathRecordSubmission message should be voided.</summary>
     public class VoidMessage : BaseMessage
     {
-        private Parameters parameters;
-
         /// <summary>Default constructor that creates a new, empty VoidMessage.</summary>
         public VoidMessage() : base("http://nchs.cdc.gov/vrdr_submission_void")
         {
-            this.parameters = new Parameters();
-            this.parameters.Id = Guid.NewGuid().ToString();
-            MessageBundle.AddResourceEntry(this.parameters, "urn:uuid:" + this.parameters.Id);
-            Header.Focus.Add(new ResourceReference("urn:uuid:" + this.parameters.Id));
         }
 
         /// <summary>
@@ -24,7 +18,6 @@ namespace VRDR
         /// <returns></returns>
         internal VoidMessage(Bundle messageBundle) : base(messageBundle)
         {
-            parameters = findEntry<Parameters>(ResourceType.Parameters);
         }
 
         /// <summary>Constructor that takes a VRDR.DeathRecord and creates a message to void that record.</summary>
@@ -33,34 +26,6 @@ namespace VRDR
         {
             this.CertificateNumber = record?.Identifier;
             this.StateIdentifier = record?.BundleIdentifier;
-        }
-
-        /// <summary>Jurisdiction-assigned death certificate number</summary>
-        public string CertificateNumber
-        {
-            get
-            {
-                return parameters.GetSingleValue<FhirString>("cert_no")?.Value;
-            }
-            set
-            {
-                parameters.Remove("cert_no");
-                parameters.Add("cert_no", new FhirString(value));
-            }
-        }
-
-        /// <summary>Jurisdiction-assigned id</summary>
-        public string StateIdentifier
-        {
-            get
-            {
-                return parameters.GetSingleValue<FhirString>("state_id")?.Value;
-            }
-            set
-            {
-                parameters.Remove("state_id");
-                parameters.Add("state_id", new FhirString(value));
-            }
         }
     }
 }

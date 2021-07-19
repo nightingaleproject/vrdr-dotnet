d1679c39e9518ea6c94802a6c8f27a8d7c624339
Saul Kravitz <skravitz@mitre.org>
2021-07-19 17:48:38 -0400

    added Global constants for CodeSystem identifiers, and pointed them to correct PHINVADS OIDs.   Should probably correct the test cases to use OIDs instead of HL7 URLs, but since the test only looks at the codes, not the system values in the codeable concepts, it works

---
+++ VRDR/CodeSystems.cs
@@ -0,0 +1,39 @@
+namespace VRDR
+{
+    /// <summary>Single definitions for CodeSystem OIDs and URLs used throughout. </summary>
+    public static class CodeSystems
+    {
+        /// <summary>SNOMEDCT.</summary>
+        public static string SCT = "http://snomed.info/sct";
+
+        /// <summary>LOINC.</summary>
+        public static string LOINC = "http://loinc.org";
+
+        /// <summary>Social Security Numbers.</summary>
+        public static string US_SSN = "http://hl7.org/fhir/sid/us-ssn";
+
+        /// <summary>PHINVADS Race and Ethnicity.</summary>
+        public static string PH_RaceAndEthnicity_CDC = "urn:oid:2.16.840.1.113883.6.238";
+        /// <summary>PHINVADS Null Flavor.</summary>
+        public static string PH_NullFlavor_HL7_V3 = "urn:oid:2.16.840.1.113883.5.1008";
+        /// <summary>PHINVADS Local Coding System.</summary>
+        public static string PH_PHINVS_CDC = "urn:oid:2.16.840.1.114222.4.5.274";
+        /// <summary>PHINVADS Yes/No.</summary>
+        public static string PH_YesNo_HL7_2x = "urn:oid:2.16.840.1.113883.12.136";
+        /// <summary>PHINVADS Place of Occurrence.</summary>
+        public static string PH_PlaceOfOccurrence_ICD_10_WHO = "urn:oid:2.16.840.1.114222.4.5.320";
+        /// <summary>PHINVADS Marital Satus.</summary>
+        public static string PH_MaritalStatus_HL7_2x = "urn:oid:2.16.840.1.113883.12.2";
+        /// <summary>USGS GNIS.</summary>
+        public static string PH_USGS_GNIS = "urn:oid:2.16.840.1.113883.6.245";
+        /// <summary>FIPS 5 2.</summary>
+        public static string PH_State_FIPS_5_2 = "urn:oid:2.16.840.1.113883.6.92";
+        /// <summary>HL7 Location Physical Type.</summary>
+        public static string HL7_location_physical_type = "http://terminology.hl7.org/CodeSystem/location-physical-type";
+        
+        /// <summary>HL7 Identifier Type.</summary>
+        public static string HL7_identifier_type = "http://terminology.hl7.org/CodeSystem/v2-0203";
+
+    }
+
+}

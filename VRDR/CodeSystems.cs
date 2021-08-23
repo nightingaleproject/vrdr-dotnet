33c3ce0754f94068c1c73d26f266b6d72f3943c9
Saul A Kravitz <37216915+saulakravitz@users.noreply.github.com>
2021-08-23 15:07:25 -0400

    added some CodeSystems used in examples, but not in code

--- VRDR/CodeSystems.cs
+++ VRDR/CodeSystems.cs
@@ -14,19 +14,19 @@ namespace VRDR
 
         /// <summary>PHINVADS Race and Ethnicity.</summary>
         public static string PH_RaceAndEthnicity_CDC = "urn:oid:2.16.840.1.113883.6.238";
-        
+
         /// <summary>PHINVADS Null Flavor.</summary>
         public static string PH_NullFlavor_HL7_V3 = "urn:oid:2.16.840.1.113883.5.1008";
-        
+
         /// <summary>HL7 V3 Null Flavor.</summary>
         public static string NullFlavor_HL7_V3 = "http://terminology.hl7.org/CodeSystem/v3-NullFlavor";
-        
+
         /// <summary>HL7 Data Absent reason.</summary>
-        public static string Data_Absent_Reason_HL7_V3 = "http://terminology.hl7.org/CodeSystem/data-absent-reason";     
-        
+        public static string Data_Absent_Reason_HL7_V3 = "http://terminology.hl7.org/CodeSystem/data-absent-reason";
+
         /// <summary>PHINVADS Local Coding System.</summary>
         public static string PH_PHINVS_CDC = "urn:oid:2.16.840.1.114222.4.5.274";
-        
+
         /// <summary>PHINVADS Yes/No.</summary>
         public static string PH_YesNo_HL7_2x = "urn:oid:2.16.840.1.113883.12.136";
 
@@ -48,17 +48,22 @@ namespace VRDR
         public static string PH_State_FIPS_5_2 = "urn:oid:2.16.840.1.113883.6.92";
         /// <summary>HL7 Location Physical Type.</summary>
         public static string HL7_location_physical_type = "http://terminology.hl7.org/CodeSystem/location-physical-type";
-        
+        /// <summary> CDC Census Occupation Code  </summary>
+        public static string PH_Occupation_CDC_Census2010 = "urn:oid:2.16.840.1.114222.4.5.314";
+        /// <summary> CDC Census Industry Code  </summary>
+        public static string PH_Industry_CDC_Census2010 = "urn:oid:2.16.840.1.114222.4.5.315";
+        /// <summary> US NPI HL7  </summary>
+        public static string PH_Industry_CDC_Census2010 = "urn:oid:2.16.840.1.114222.4.5.315";
         /// <summary>HL7 Identifier Type.</summary>
-        public static string HL7_identifier_type = "http://terminology.hl7.org/CodeSystem/v2-0203";
+        public static string US_NPI_HL7 = "http://hl7.org/fhir/sid/us-npi";
 
        /// <summary>HL7 Organization Type.</summary>
         public static string HL7_organization_type =  "http://terminology.hl7.org/CodeSystem/organization-type";
-       
+
 
         /// <summary>PHINVADS HL7 RoleCode.</summary>
         public static string PH_RoleCode_HL7_V3 = "urn:oid:2.16.840.1.113883.5.111";
-        
+
         /// <summary>HL7 RoleCode.</summary>
         public static string RoleCode_HL7_V3 = "http://terminology.hl7.org/CodeSystem/v3-RoleCode";
 

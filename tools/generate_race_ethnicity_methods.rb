137b889f09d606ec7301b997338f5e469861e4e1
Pete K <krautscheid@mitre.org>
2022-05-03 12:02:05 -0400

    Update helper script with description and correctly formatted code

--- tools/generate_race_ethnicity_methods.rb
+++ tools/generate_race_ethnicity_methods.rb
@@ -1,98 +1,88 @@
-# This script takes the JSON files that are generated as part of the VRDR IG and creates an output
-# file with static URL strings for each StructureDefinition, Extension, and IG HTML page
+# This script generates code for all the race and ethnicity methods in DeathRecord.cs
 #
-# Usage: ruby tools/generate_concept_mappings_from_VRDR_IG.rb <path-to-json-files> > VRDR/URLs.cs
-#
-# If you need to generate the concept map JSON files, first install sushi (https://github.com/FHIR/sushi) then
-#
-#     git clone https://github.com/HL7/vrdr.git
-#     cd vrdr
-#     sushi
-#
-# The JSON files will be built in vrdr/fsh-generated/resources/
+# Usage: ruby tools/generate_race_ethnicity_methods.rb
 
 require 'json'
 require 'erb'
 require 'byebug'
 template = "
-  /// <summary>#descrip#.</summary>
-  /// <value>#descrip#. A Dictionary representing a code, containing the following key/value pairs:
-  /// <para>\"code\" - the code</para>
-  /// <para>\"system\" - the code system this code belongs to</para>
-  /// <para>\"display\" - a human readable meaning of the code</para>
-  /// </value>
-  /// <example>
-  /// <para>// Setter:</para>
-  /// <para>Dictionary&lt;string, string&gt; racecode = new Dictionary&lt;string, string&gt;();</para>
-  /// <para>racecode.Add(\"code\", \"300\");</para>
-  /// <para>racecode.Add(\"system\", CodeSystems.RaceCode);</para>
-  /// <para>racecode.Add(\"display\", \"African\");</para>
-  /// <para>ExampleDeathRecord.*name* = racecode;</para>
-  /// <para>// Getter:</para>
-  /// <para>Console.WriteLine($\"#descrip#: {ExampleDeathRecord.*name*['display']}\");</para>
-  /// </example>
-  [Property(\"*name*\", Property.Types.Dictionary, \"Coded Content\", \"#descrip#.\", true, IGURL.CodedRaceAndEthnicity, false, 34)]
-  [PropertyParam(\"code\", \"The code used to describe this concept.\")]
-  [PropertyParam(\"system\", \"The relevant code system.\")]
-  [PropertyParam(\"display\", \"The human readable version of this code.\")]
-  [FHIRPath(\"Bundle.entry.resource.where($this is Observation).where(code.coding.code='codedraceandethnicity')\", \"\")]
-  public Dictionary<string, string> *name*
-  {
-      get
-      {
-          if (CodedRaceandEthnicityObs != null)
-          {
-              Observation.ComponentComponent racecode = CodedRaceandEthnicityObs.Component.FirstOrDefault(c => c.Code.Coding[0].Code == \"@component@\");
-              if (racecode != null && racecode.Value != null && racecode.Value as CodeableConcept != null)
-              {
-                  return CodeableConceptToDict((CodeableConcept)racecode.Value);
-              }
-          }
-          return EmptyCodeableDict();
-      }
-      set
-      {
-          if (CodedRaceandEthnicityObs == null)
-          {
-              CreateCodedRaceandEthnicityObs();
-          }
-          CodedRaceandEthnicityObs.Component.RemoveAll(c => c.Code.Coding[0].Code == \"@component@\");
-          Observation.ComponentComponent component = new Observation.ComponentComponent();
-          component.Code = new CodeableConcept(CodeSystems.ComponentCode, \"@component@\", \"&descripnorace&\", null);
-          component.Value = DictToCodeableConcept(value);
-          CodedRaceandEthnicityObs.Component.Add(component);
-      }
-  }
+        /// <summary>#descrip#.</summary>
+        /// <value>#descrip#. A Dictionary representing a code, containing the following key/value pairs:
+        /// <para>\"code\" - the code</para>
+        /// <para>\"system\" - the code system this code belongs to</para>
+        /// <para>\"display\" - a human readable meaning of the code</para>
+        /// </value>
+        /// <example>
+        /// <para>// Setter:</para>
+        /// <para>Dictionary&lt;string, string&gt; racecode = new Dictionary&lt;string, string&gt;();</para>
+        /// <para>racecode.Add(\"code\", \"300\");</para>
+        /// <para>racecode.Add(\"system\", CodeSystems.RaceCode);</para>
+        /// <para>racecode.Add(\"display\", \"African\");</para>
+        /// <para>ExampleDeathRecord.*name* = racecode;</para>
+        /// <para>// Getter:</para>
+        /// <para>Console.WriteLine($\"#descrip#: {ExampleDeathRecord.*name*['display']}\");</para>
+        /// </example>
+        [Property(\"*name*\", Property.Types.Dictionary, \"Coded Content\", \"#descrip#.\", true, IGURL.CodedRaceAndEthnicity, false, 34)]
+        [PropertyParam(\"code\", \"The code used to describe this concept.\")]
+        [PropertyParam(\"system\", \"The relevant code system.\")]
+        [PropertyParam(\"display\", \"The human readable version of this code.\")]
+        [FHIRPath(\"Bundle.entry.resource.where($this is Observation).where(code.coding.code='codedraceandethnicity')\", \"\")]
+        public Dictionary<string, string> *name*
+        {
+            get
+            {
+                if (CodedRaceandEthnicityObs != null)
+                {
+                    Observation.ComponentComponent racecode = CodedRaceandEthnicityObs.Component.FirstOrDefault(c => c.Code.Coding[0].Code == \"@component@\");
+                    if (racecode != null && racecode.Value != null && racecode.Value as CodeableConcept != null)
+                    {
+                        return CodeableConceptToDict((CodeableConcept)racecode.Value);
+                    }
+                }
+                return EmptyCodeableDict();
+            }
+            set
+            {
+                if (CodedRaceandEthnicityObs == null)
+                {
+                    CreateCodedRaceandEthnicityObs();
+                }
+                CodedRaceandEthnicityObs.Component.RemoveAll(c => c.Code.Coding[0].Code == \"@component@\");
+                Observation.ComponentComponent component = new Observation.ComponentComponent();
+                component.Code = new CodeableConcept(CodeSystems.ComponentCode, \"@component@\", \"&descripnorace&\", null);
+                component.Value = DictToCodeableConcept(value);
+                CodedRaceandEthnicityObs.Component.Add(component);
+            }
+        }
 
-  /// <summary>#descrip#  Helper</summary>
-  /// <value>#descrip# Helper.</value>
-  /// <example>
-  /// <para>// Setter:</para>
-  /// <para>ExampleDeathRecord.*name*Helper = VRDR.ValueSets.RaceCode.African ;</para>
-  /// <para>// Getter:</para>
-  /// <para>Console.WriteLine($\"#descrip#: {ExampleDeathRecord.*name*}\");</para>
-  /// </example>
-  [Property(\"#descrip# Helper\", Property.Types.String, \"Coded Content\", \"#descrip#.\", true, IGURL.CodedRaceAndEthnicity, false, 34)]
-  [PropertyParam(\"code\", \"The code used to describe this concept.\")]
-  [FHIRPath(\"Bundle.entry.resource.where($this is Observation).where(code.coding.code='codedraceandethnicity')\", \"\")]
-  public string *name*Helper
-  {
-      get
-      {
-          if (*name*.ContainsKey(\"code\"))
-          {
-              return *name*[\"code\"];
-          }
-          return null;
-      }
-      set
-      {
-          SetCodeValue(\"*name*\", value, VRDR.ValueSets.RaceCode.Codes);
-      }
-  }
+        /// <summary>#descrip#  Helper</summary>
+        /// <value>#descrip# Helper.</value>
+        /// <example>
+        /// <para>// Setter:</para>
+        /// <para>ExampleDeathRecord.*name*Helper = VRDR.ValueSets.RaceCode.African ;</para>
+        /// <para>// Getter:</para>
+        /// <para>Console.WriteLine($\"#descrip#: {ExampleDeathRecord.*name*Helper}\");</para>
+        /// </example>
+        [Property(\"#descrip# Helper\", Property.Types.String, \"Coded Content\", \"#descrip#.\", true, IGURL.CodedRaceAndEthnicity, false, 34)]
+        [PropertyParam(\"code\", \"The code used to describe this concept.\")]
+        [FHIRPath(\"Bundle.entry.resource.where($this is Observation).where(code.coding.code='codedraceandethnicity')\", \"\")]
+        public string *name*Helper
+        {
+            get
+            {
+                if (*name*.ContainsKey(\"code\"))
+                {
+                    return *name*[\"code\"];
+                }
+                return null;
+            }
+            set
+            {
+                SetCodeValue(\"*name*\", value, VRDR.ValueSets.RaceCode.Codes);
+            }
+        }
 
-
-  "
+"
 
 race_methods = ["First Edited", "Second Edited","Third Edited", "Fourth Edited","Fifth Edited", "Sixth Edited","Seventh Edited", "Eighth Edited",
                          "First American Indian", "Second American Indian",

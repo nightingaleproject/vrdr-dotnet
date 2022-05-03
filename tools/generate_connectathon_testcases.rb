f2dcc9d079b13f2048a98be7f8f960457892d187
Saul Kravitz <skravitz@mitre.org>
2022-05-03 10:10:56 -0400

    added documentation

--- tools/generate_connectathon_testcases.rb
+++ tools/generate_connectathon_testcases.rb
@@ -1,3 +1,16 @@
+# This script takes an excel file provided by NCHS (see FHIRTestCases*.xlsx in this directory)
+# and produces csharp code for inclusion in the VRDR dot net library.
+# The process is semi-automated.
+# Other limitations/shortcuts taken:
+#   - the number of testcases is hardcoded to 3, so adding more test cases would involve changes to the code
+#   - the rows with the name information are hardcoded, rather than searched-for/found in the file, so changes to the structure of the input would require code changes.
+#
+# Usage: ruby tools/generate_connectathon_testcases.rb <path-to-excel-files>
+#
+# Output is produced in the generated subdirectory and needs to be pasted into the Connectahon.cs file.  The Files are named connectathon_<name>.cs
+#
+#
+
 require "pry"
 require "roo"
 require "byebug"
@@ -46,7 +59,7 @@ record_col.each do |col|
 "
     for row in (2..128) do
         fieldName = xlsx.cell(row,IJEName)
-        fieldValue = xlsx.cell(row,col).to_s.split('(').first
+        fieldValue = xlsx.cell(row,col).to_s.split('(').first   # spreadsheet includes parenthetical information for humans that we ignore
         next if fieldValue == "␢"
         next if fieldValue == nil
         out.puts "\t\tije.#{fieldName} = \"#{fieldValue.strip}\";"

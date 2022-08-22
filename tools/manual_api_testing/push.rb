87e2b162def261f13cf1571a86817255f1d15c74
Pete K <krautscheid@mitre.org>
2022-08-22 11:46:06 -0400

    Support pushing multiple messages to API in single invocation

--- tools/manual_api_testing/push.rb
+++ tools/manual_api_testing/push.rb
@@ -1,5 +1,5 @@
-# Submit a message to the NVSS API; takes as arguments the jurisdiction to submit for (e.g., MA) and the file
-# to send (must be a valid FHIR message)
+# Submit messages to the NVSS API; takes as arguments the jurisdiction to submit for (e.g., MA) and the files
+# to send (must be valid FHIR messages)
 
 require 'oauth2'
 require 'active_support/time'
@@ -11,11 +11,9 @@ if jurisdiction.nil? || !jurisdiction.match(/^[A-Z][A-Z]$/)
 end
 
 if ARGV.length < 1
-  puts "Please provide a file to submit"
+  puts "Please provide files to submit"
   exit
 end
-
-body = ARGF.to_a.join
 
 client_id = File.read('clientid.txt')
 client_secret = File.read('clientsecret.txt')
@@ -29,8 +27,16 @@ client = OAuth2::Client.new(client_id,
 
 token = client.password.get_token(username, password)
 
-response = token.post("/OSELS/NCHS/NVSSFHIRAPI/#{jurisdiction}/Bundles",
-                      headers: { 'Content-Type' => 'application/json' },
-                      body: body)
+ARGV.each do |filename|
 
-puts "Server response: #{response.status}"
+  body = File.read(filename)
+
+  puts "Sending #{filename}"
+
+  response = token.post("/OSELS/NCHS/NVSSFHIRAPI/#{jurisdiction}/Bundles",
+                        headers: { 'Content-Type' => 'application/json' },
+                        body: body)
+
+  puts "Server response: #{response.status}"
+
+end

d31e30d67d2457c1b91e75f8445871d88af97c04
smacadam <smacadam@users.noreply.github.com>
2022-09-14 16:14:42 +0000

    deploy: 225c662484a11b76c9c66f8fb1290dba46e944a1

--- search/functions_7.js
+++ search/functions_7.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['ijefield_0',['IJEField',['../class_v_r_d_r_1_1_i_j_e_field.html#a2a62061a13e4e6e791a67fccce19a5a9',1,'VRDR::IJEField']]],
   ['ijemortality_1',['IJEMortality',['../class_v_r_d_r_1_1_i_j_e_mortality.html#aa68db16c68d426e958e098cff59113cc',1,'VRDR.IJEMortality.IJEMortality(DeathRecord record, bool validate=true)'],['../class_v_r_d_r_1_1_i_j_e_mortality.html#afeedc8862102cb256cc8cc94ae6744ec',1,'VRDR.IJEMortality.IJEMortality(string ije, bool validate=true)'],['../class_v_r_d_r_1_1_i_j_e_mortality.html#a44b1511d58dd5c67a115c797a9153738',1,'VRDR.IJEMortality.IJEMortality()']]],
-  ['issue_2',['Issue',['../class_v_r_d_r_1_1_issue.html#aa4a6938ce1ae1db991c8456b545dcac4',1,'VRDR::Issue']]]
+  ['injuryincidenttimeset_2',['InjuryIncidentTimeSet',['../class_v_r_d_r_1_1_death_record.html#afb16bb55917b951789fde764d19e8f37',1,'VRDR::DeathRecord']]],
+  ['issue_3',['Issue',['../class_v_r_d_r_1_1_issue.html#aa4a6938ce1ae1db991c8456b545dcac4',1,'VRDR::Issue']]]
 ];

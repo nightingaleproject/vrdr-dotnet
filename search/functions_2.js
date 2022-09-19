e051e10f6b1f64c5210d1079f608c8073fb100e1
pkmitre <pkmitre@users.noreply.github.com>
2022-09-19 16:15:22 +0000

    deploy: 4151bbd3d69d5d386c7055d4f786c3e7322f23a0

--- search/functions_2.js
+++ search/functions_2.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['causeofdeathcodingmessage_0',['CauseOfDeathCodingMessage',['../class_v_r_d_r_1_1_cause_of_death_coding_message.html#af612e5a860e5578aa88ad451a58a31e2',1,'VRDR::CauseOfDeathCodingMessage']]],
+  ['causeofdeathcodingmessage_0',['CauseOfDeathCodingMessage',['../class_v_r_d_r_1_1_cause_of_death_coding_message.html#af612e5a860e5578aa88ad451a58a31e2',1,'VRDR.CauseOfDeathCodingMessage.CauseOfDeathCodingMessage(DeathRecord record)'],['../class_v_r_d_r_1_1_cause_of_death_coding_message.html#adba833ff3efca3e7dd035ca96bdf3ee3',1,'VRDR.CauseOfDeathCodingMessage.CauseOfDeathCodingMessage(BaseMessage messageToCode)'],['../class_v_r_d_r_1_1_cause_of_death_coding_message.html#a50df466215654b01b08ce2f552f1cdf4',1,'VRDR.CauseOfDeathCodingMessage.CauseOfDeathCodingMessage(string messageId, string destination, string status, string source=&quot;http://nchs.cdc.gov/vrdr_submission&quot;)']]],
   ['causeofdeathcodingupdatemessage_1',['CauseOfDeathCodingUpdateMessage',['../class_v_r_d_r_1_1_cause_of_death_coding_update_message.html#ad46ccecf0731b8e299e803874e0c30fe',1,'VRDR::CauseOfDeathCodingUpdateMessage']]],
   ['countrycodetocountryname_2',['CountryCodeToCountryName',['../class_v_r_d_r_1_1_mortality_data.html#ad7efdd226ad1a37d8ef417f60399bb11',1,'VRDR::MortalityData']]],
   ['countrynametocountrycode_3',['CountryNameToCountryCode',['../class_v_r_d_r_1_1_mortality_data.html#a58e69f802dfeb8134c0fca2aeae9fbec',1,'VRDR::MortalityData']]],

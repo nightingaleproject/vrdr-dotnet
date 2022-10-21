af66b134b88f1653b04c21c314bfbf73f68a2c46
Pete K <krautscheid@mitre.org>
2022-10-21 17:05:38 -0400

    Add support for creating and POSTing batches for upload to the API

--- VRDR.Client/Client.cs
+++ VRDR.Client/Client.cs
@@ -1,10 +1,8 @@
-﻿using System;
 using System.Text;
-using System.Threading.Tasks;
-using System.Net;
-using System.Net.Http;
 using System.Net.Http.Headers;
 using Newtonsoft.Json.Linq;
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Serialization;
 using VRDR;
 
 
@@ -60,7 +58,7 @@ public class Client
             }
             response = await client.GetAsync(this.Url);
         }
-        
+
         return response;
     }
 
@@ -100,6 +98,63 @@ public class Client
         return response;
     }
 
+    public static string CreateBulkUploadPayload(IEnumerable<BaseMessage> messages, string url, bool prettyPrint = false)
+    {
+        Bundle payload = new Bundle();
+        payload.Id = Guid.NewGuid().ToString();
+        payload.Type = Bundle.BundleType.Batch;
+        payload.Timestamp = DateTime.Now;
+        foreach(BaseMessage message in messages)
+        {
+            Bundle.EntryComponent entry = new Bundle.EntryComponent();
+            entry.Resource = (Bundle)message;
+            entry.Request = new Bundle.RequestComponent();
+            entry.Request.Method = Bundle.HTTPVerb.POST;
+            entry.Request.Url = url;
+            payload.Entry.Add(entry);
+        }
+        return payload.ToJson(new FhirJsonSerializationSettings { Pretty = prettyPrint, AppendNewLine = prettyPrint });
+    }
+
+    // PostMessageAsync POSTS an array message to the NVSS FHIR API server for processing using bulk upload
+    public async Task<HttpResponseMessage[]> PostMessagesAsync(IEnumerable<BaseMessage> messages, string url)
+    {
+        string json = CreateBulkUploadPayload(messages, url);
+        StringContent data = new StringContent(json, Encoding.UTF8, "application/json");
+
+        // if testing against the NVSS FHIR API server, add the authentication token
+        if (!this.LocalTesting){
+            if (String.IsNullOrEmpty(this.Token))
+            {
+                HttpResponseMessage authResponse = await RefreshAccessTokenAsync();
+                // return authentication error
+                if (!authResponse.IsSuccessStatusCode)
+                {
+                    return Enumerable.Repeat(authResponse, messages.Count()).ToArray();
+                }
+            }
+        }
+        var response = client.PostAsync(this.Url, data).Result;
+
+        // check if the token expired, refresh and try again
+        if (!response.IsSuccessStatusCode)
+        {
+            HttpResponseMessage authRetry = await RefreshAccessTokenAsync();
+            // return authentication error
+            if (!authRetry.IsSuccessStatusCode)
+            {
+                return Enumerable.Repeat(authRetry, messages.Count()).ToArray();
+            }
+            response = await client.PostAsync(this.Url, data);
+        }
+
+        // At this point we have an overall HTTP response plus, if the request succeeded, one internal response for each
+        // message in the bulk submission. To process this we first check for success and, if the request did succeed,
+        // break out the internal responses, otherwise return the overall response as the response for each message
+        // TODO NOTFORCHECKIN: Implement this!
+        return Enumerable.Repeat(response, messages.Count()).ToArray();
+    }
+
     private async Task<HttpResponseMessage> RefreshAccessTokenAsync(){
     // clear the authentication headers
     client.DefaultRequestHeaders.Authorization = null;
@@ -111,11 +166,10 @@ public class Client
             if (!String.IsNullOrEmpty(content))
             {
                 JObject json = JObject.Parse(content);
-                if (json["access_token"] != null)
+                this.Token = json["access_token"]?.ToString();
+                if (this.Token != null)
                 {
-                    this.Token = json["access_token"].ToString();
-                    string authorization = this.Token;
-                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", authorization);
+                    client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", this.Token);
                 }
             }
             return response;
@@ -135,7 +189,7 @@ public class Client
         parameters.Add("client_secret", this.Credentials.ClientSecret);
         parameters.Add("username", this.Credentials.Username);
         parameters.Add("password", this.Credentials.Pass);
-        
+
         var request = new HttpRequestMessage(HttpMethod.Post, this.Credentials.Url){Content = new FormUrlEncodedContent(parameters)};
         var response = await client.SendAsync(request);
 

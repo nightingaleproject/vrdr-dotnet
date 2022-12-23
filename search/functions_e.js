0e4026759c1671853bf5859bd8716f075183b6e3
smacadam <smacadam@users.noreply.github.com>
2022-12-23 21:42:44 +0000

    deploy: e81f135127219f6ba39ed6625f99ccf258e8a38a

--- search/functions_e.js
+++ search/functions_e.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['updatemessagebundlerecord_0',['UpdateMessageBundleRecord',['../class_v_r_d_r_1_1_base_message.html#a5d3bdc84ad30f9df47d2251519909503',1,'VRDR::BaseMessage']]]
+  ['updategivenhumanname_0',['updateGivenHumanName',['../class_v_r_d_r_1_1_death_record.html#aec87893df7ca5bfb6614c48a1b3523f3',1,'VRDR::DeathRecord']]],
+  ['updatemessagebundlerecord_1',['UpdateMessageBundleRecord',['../class_v_r_d_r_1_1_base_message.html#a5d3bdc84ad30f9df47d2251519909503',1,'VRDR::BaseMessage']]]
 ];

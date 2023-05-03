fefbcafaae1b1bb32ba324349b247ab6c170235e
johnarleyburns <johnarleyburns@users.noreply.github.com>
2023-05-03 17:57:08 +0000

    deploy: 945697c0c95093dcad8080511e8e8e903afeb3a9

--- search/functions_6.js
+++ search/functions_6.js
@@ -5,12 +5,14 @@ var searchData=
   ['getbundle_2',['GetBundle',['../class_v_r_d_r_1_1_death_record.html#a5b24253082f8f4c0e8bbe898febc2420',1,'VRDR::DeathRecord']]],
   ['getcauseofdeathcodedcontentbundle_3',['GetCauseOfDeathCodedContentBundle',['../class_v_r_d_r_1_1_death_record.html#ae0e502756952456ee094a1e530dcafef',1,'VRDR::DeathRecord']]],
   ['getcomposition_4',['GetComposition',['../class_v_r_d_r_1_1_death_record.html#a73607d76f91f78315915e9ec933dff9e',1,'VRDR::DeathRecord']]],
-  ['getdeathcertificatedocumentbundle_5',['GetDeathCertificateDocumentBundle',['../class_v_r_d_r_1_1_death_record.html#af9985739042d9e3ac6c06f6f19a2e80f',1,'VRDR::DeathRecord']]],
-  ['getdeathrecordfrommessage_6',['GetDeathRecordFromMessage',['../class_v_r_d_r_1_1_base_message.html#a9bdb5e46ee7dc54615c998f5df5dfe67',1,'VRDR::BaseMessage']]],
-  ['getdemographiccodedcontentbundle_7',['GetDemographicCodedContentBundle',['../class_v_r_d_r_1_1_death_record.html#af2aa66beaeb8ef80aaae0b24b730af77',1,'VRDR::DeathRecord']]],
-  ['getfirst_8',['GetFirst',['../class_v_r_d_r_1_1_death_record.html#aa52277443735efe7ddadd9c0a50fb889',1,'VRDR::DeathRecord']]],
-  ['getitypedelement_9',['GetITypedElement',['../class_v_r_d_r_1_1_death_record.html#a14d009a92d42937e26e47ba8c324e97a',1,'VRDR::DeathRecord']]],
-  ['getlast_10',['GetLast',['../class_v_r_d_r_1_1_death_record.html#aced32110e46501fbc5a8693605befb5a',1,'VRDR::DeathRecord']]],
-  ['getmessageresponsesasync_11',['GetMessageResponsesAsync',['../class_v_r_d_r_1_1_client.html#aa6bd39ea26087937279709b49c3e9719',1,'VRDR::Client']]],
-  ['getmortalityrosterbundle_12',['GetMortalityRosterBundle',['../class_v_r_d_r_1_1_death_record.html#adc5285ddb7527891c4f1bbee87dd5104',1,'VRDR::DeathRecord']]]
+  ['getdateofdeathpronouncementobs_5',['GetDateOfDeathPronouncementObs',['../class_v_r_d_r_1_1_death_record.html#a33940f95792f6612e99efbe5f8598482',1,'VRDR::DeathRecord']]],
+  ['getdeathcertificatedocumentbundle_6',['GetDeathCertificateDocumentBundle',['../class_v_r_d_r_1_1_death_record.html#af9985739042d9e3ac6c06f6f19a2e80f',1,'VRDR::DeathRecord']]],
+  ['getdeathrecordfrommessage_7',['GetDeathRecordFromMessage',['../class_v_r_d_r_1_1_base_message.html#a9bdb5e46ee7dc54615c998f5df5dfe67',1,'VRDR::BaseMessage']]],
+  ['getdemographiccodedcontentbundle_8',['GetDemographicCodedContentBundle',['../class_v_r_d_r_1_1_death_record.html#af2aa66beaeb8ef80aaae0b24b730af77',1,'VRDR::DeathRecord']]],
+  ['getfirst_9',['GetFirst',['../class_v_r_d_r_1_1_death_record.html#aa52277443735efe7ddadd9c0a50fb889',1,'VRDR::DeathRecord']]],
+  ['getitypedelement_10',['GetITypedElement',['../class_v_r_d_r_1_1_death_record.html#a14d009a92d42937e26e47ba8c324e97a',1,'VRDR::DeathRecord']]],
+  ['getlast_11',['GetLast',['../class_v_r_d_r_1_1_death_record.html#aced32110e46501fbc5a8693605befb5a',1,'VRDR::DeathRecord']]],
+  ['getmessageresponsesasync_12',['GetMessageResponsesAsync',['../class_v_r_d_r_1_1_client.html#aa6bd39ea26087937279709b49c3e9719',1,'VRDR::Client']]],
+  ['getmortalityrosterbundle_13',['GetMortalityRosterBundle',['../class_v_r_d_r_1_1_death_record.html#adc5285ddb7527891c4f1bbee87dd5104',1,'VRDR::DeathRecord']]],
+  ['getorcreatedateofdeathpronouncementobs_14',['GetOrCreateDateOfDeathPronouncementObs',['../class_v_r_d_r_1_1_death_record.html#a1c36219f328e01733c483e732057b10d',1,'VRDR::DeathRecord']]]
 ];

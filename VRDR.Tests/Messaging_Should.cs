b66e03d9cb64ad99bb0b62abbabb34956d6542ba
Thomas Nguyen <118298834+consultantusa@users.noreply.github.com>
2023-05-10 17:00:54 -0500

    Update Messaging_Should.cs

--- VRDR.Tests/Messaging_Should.cs
+++ VRDR.Tests/Messaging_Should.cs
@@ -507,7 +507,7 @@ namespace VRDR.Tests
             Assert.Equal("https://example.org/jurisdiction/endpoint", message.MessageDestination);
             Assert.Equal((uint)123, message.CertNo);
             Assert.Equal((uint)2022, message.DeathYear);
-          //  Assert.Equal("000000000500", message.StateAuxiliaryId);
+            Assert.Equal("000000000500", message.StateAuxiliaryId);
             Assert.Equal("2022YC000123", message.NCHSIdentifier);
             Assert.Equal("378888", message.CodedMessageId);
             Assert.Equal("T27.3", message.DeathRecord.AutomatedUnderlyingCOD);

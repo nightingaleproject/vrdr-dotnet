039c1bb52c6a3fbfe998381362aa3ef191427fae
Thomas Nguyen <118298834+consultantusa@users.noreply.github.com>
2023-05-15 13:06:47 -0500

    Update Connectathon.cs

--- VRDR/Connectathon.cs
+++ VRDR/Connectathon.cs
@@ -109,8 +109,8 @@ namespace VRDR
             ije.PREG = "8";//2";
             ije.PREG_BYPASS = "0";
             ije.CERTL = "D";
-            ije.SUR_MO;// = "01";
-            ije.SUR_DY;// = "10";
+            ije.SUR_MO=null;// = "01";
+            ije.SUR_DY="";// = "10";
             ije.SUR_YR;// = "2022";
             ije.STNUM_R = "4437";
             ije.PREDIR_R = "North";

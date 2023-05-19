337f9e304f4ebe0b59e375eb3ba273404a3a0fcd
Thomas Nguyen <118298834+consultantusa@users.noreply.github.com>
2023-05-18 20:15:59 -0500

    Update IJEMortality.cs

--- VRDR/IJEMortality.cs
+++ VRDR/IJEMortality.cs
@@ -741,11 +741,26 @@ namespace VRDR
             }
             catch (KeyNotFoundException)
             {
-                if (ijeField == "COD") ijeField = "Death County";
-                if (ijeField == "COD1A") ijeField = "Cause of Death-1A";
-                if (ijeField == "COD1B") ijeField = "Cause of Death-1B";
-                if (ijeField == "COD1C") ijeField = "Cause of Death-1C";
-                if (ijeField == "COD1D") ijeField = "Cause of Death-1D";
+                switch (ijeField)
+                {
+                    case "COD":
+                        ijeField = "County of Death";
+                        break;
+                    case "COD1A":
+                        ijeField = "Cause of Death-1A";
+                        break;
+                    case "COD1B":
+                        ijeField = "Cause of Death-1B";
+                        break;
+                    case "COD1C":
+                        ijeField = "Cause of Death-1C";
+                        break;
+                    case "COD1D":
+                        ijeField = "Cause of Death-1D";
+                        break;
+                    default:
+                        break;
+                }
                 validationErrors.Add($"Error: Unable to find IJE {ijeField} mapping for FHIR {fhirField} field value '{fhirCode}'");
                 return "";
             }

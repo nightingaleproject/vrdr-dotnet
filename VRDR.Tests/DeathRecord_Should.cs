0ce346a318ab3e8620574f5b90de8a3f9f58fa44
Thomas Nguyen <118298834+consultantusa@users.noreply.github.com>
2023-05-30 15:19:26 -0500

    Update DeathRecord_Should.cs

--- VRDR.Tests/DeathRecord_Should.cs
+++ VRDR.Tests/DeathRecord_Should.cs
@@ -54,8 +54,8 @@ namespace VRDR.Tests
           Exception ex = Assert.Throws<System.ArgumentOutOfRangeException>(() => new IJEMortality(dr).DPLACE);
             
              Console.WriteLine("*** ex.Message ***" + ex.Message.Substring(96));
-         //   Assert.Equal("Error: Unable to find IJE DPLACE mapping for FHIR DeathLocationType field value 440081000124100x", ex.Message.Substring(96));
-            Assert.IsTrue(ex.Message.Contains("Error: Unable to find IJE DPLACE mapping for FHIR DeathLocationType field value"));
+            Assert.Equal("Error: Unable to find IJE DPLACE mapping for FHIR DeathLocationType field value 440081000124100x", ex.Message.Substring(96, 196));
+        //    Assert.IsTrue(ex.Message.Contains("Error: Unable to find IJE DPLACE mapping for FHIR DeathLocationType field value"));
             
 //            Assert.Equal("The given input does not appear to be a valid XML or JSON FHIR record.", ex.Message);
 //            DeathRecord dr = new DeathRecord(File.ReadAllText(FixturePath("fixtures/json/DeathRecordWrongDplaceCode.json")));

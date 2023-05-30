fdcef9ed31fccec15096592e8c3d7cba59823fd9
Thomas Nguyen <118298834+consultantusa@users.noreply.github.com>
2023-05-30 14:46:29 -0500

    Update DeathRecord_Should.cs

--- VRDR.Tests/DeathRecord_Should.cs
+++ VRDR.Tests/DeathRecord_Should.cs
@@ -53,7 +53,7 @@ namespace VRDR.Tests
             
           Exception ex = Assert.Throws<System.ArgumentOutOfRangeException>(() => new IJEMortality(dr).DPLACE);
             
-             Console.WriteLine("*** ex.Message length ***" + ex.Message.Length);
+             Console.WriteLine("*** ex.Message ***" + ex.Message.Substring(120));
             
 //            Assert.Equal("The given input does not appear to be a valid XML or JSON FHIR record.", ex.Message);
 //            DeathRecord dr = new DeathRecord(File.ReadAllText(FixturePath("fixtures/json/DeathRecordWrongDplaceCode.json")));

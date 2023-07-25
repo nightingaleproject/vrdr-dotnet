077ee24785aa3d1fd3fb2162df08467e74eef755
johnarleyburns <johnarleyburns@users.noreply.github.com>
2023-07-25 17:34:10 +0000

    deploy: 68c8e033d690de05be162091bc6d2af0c22abcee

--- search/properties_6.js
+++ search/properties_6.js
@@ -1,5 +1,6 @@
 var searchData=
 [
-  ['givennames_0',['GivenNames',['../class_v_r_d_r_1_1_death_record.html#a478852f212fc67cfab62983820498292',1,'VRDR::DeathRecord']]],
-  ['gname_1',['GNAME',['../class_v_r_d_r_1_1_i_j_e_mortality.html#aa2b7e77640aa8f79b56f19a561ace75f',1,'VRDR::IJEMortality']]]
+  ['gender_0',['Gender',['../class_v_r_d_r_1_1_death_record.html#a9825ec286027f44aa3f86b65d1c16155',1,'VRDR::DeathRecord']]],
+  ['givennames_1',['GivenNames',['../class_v_r_d_r_1_1_death_record.html#a478852f212fc67cfab62983820498292',1,'VRDR::DeathRecord']]],
+  ['gname_2',['GNAME',['../class_v_r_d_r_1_1_i_j_e_mortality.html#aa2b7e77640aa8f79b56f19a561ace75f',1,'VRDR::IJEMortality']]]
 ];

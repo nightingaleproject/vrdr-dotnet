b06526fb7b8f1ddd4545d7b07b9040313538bb03
whichwit <whichwit@users.noreply.github.com>
2024-04-19 14:06:04 +0000

    deploy: c4d69641dbfe879bda4ca57df250ccd4ffca5a7c

--- search/all_6.js
+++ search/all_6.js
@@ -1,9 +1,9 @@
 var searchData=
 [
-  ['6_2023_0',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.4&quot; &gt;4.0.4&lt;/a&gt; (2023-6-23)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md40',1,'']]],
-  ['6_2026_1',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.5&quot; &gt;4.0.5&lt;/a&gt; (2023-6-26)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md38',1,'']]],
-  ['6_2029_2',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.7&quot; &gt;4.0.7&lt;/a&gt; (2023-6-29)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md33',1,'']]],
-  ['6_204_200_206_20a_202023_206_2027_3',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.6&quot; &gt;4.0.6&lt;/a&gt; (2023-6-27)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md36',1,'']]],
-  ['6_204_201_206_20a_202023_2010_2010_4',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.1.6&quot; &gt;4.1.6&lt;/a&gt; (2023-10-10)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md6',1,'']]],
-  ['6_207_5',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.3&quot; &gt;4.0.3&lt;/a&gt; (2023-6-7)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md43',1,'']]]
+  ['6_2023_0',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.4&quot; &gt;4.0.4&lt;/a&gt; (2023-6-23)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md43',1,'']]],
+  ['6_2026_1',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.5&quot; &gt;4.0.5&lt;/a&gt; (2023-6-26)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md41',1,'']]],
+  ['6_2029_2',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.7&quot; &gt;4.0.7&lt;/a&gt; (2023-6-29)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md36',1,'']]],
+  ['6_204_200_206_20a_202023_206_2027_3',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.6&quot; &gt;4.0.6&lt;/a&gt; (2023-6-27)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md39',1,'']]],
+  ['6_204_201_206_20a_202023_2010_2010_4',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.1.6&quot; &gt;4.1.6&lt;/a&gt; (2023-10-10)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md9',1,'']]],
+  ['6_207_5',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.3&quot; &gt;4.0.3&lt;/a&gt; (2023-6-7)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md46',1,'']]]
 ];

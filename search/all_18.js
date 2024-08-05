0c23e09023e1c8a11ed00df1ca2463e92a3c78c7
whichwit <whichwit@users.noreply.github.com>
2024-08-05 18:56:14 +0000

    deploy: f3f307b85b09097739890f7ddfe08fcc4f07d017

--- search/all_18.js
+++ search/all_18.js
@@ -1,18 +1,18 @@
 var searchData=
 [
   ['occup_0',['OCCUP',['../class_v_r_d_r_1_1_i_j_e_mortality.html#a0c3b697af3301bac0d74f9382eabf8f2',1,'VRDR::IJEMortality']]],
-  ['occup_20tt_20property_20tt_1',['OCCUP &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1145',1,'']]],
+  ['occup_20tt_20property_20tt_1',['OCCUP &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1148',1,'']]],
   ['occupc_2',['OCCUPC',['../class_v_r_d_r_1_1_i_j_e_mortality.html#a7e652f58c9007bf7b9e329987302cc7b',1,'VRDR::IJEMortality']]],
-  ['occupc_20tt_20property_20tt_3',['OCCUPC &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1147',1,'']]],
+  ['occupc_20tt_20property_20tt_3',['OCCUPC &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1150',1,'']]],
   ['occupc4_4',['OCCUPC4',['../class_v_r_d_r_1_1_i_j_e_mortality.html#ab7ecbfa1ac98d380cfb0881cf7ce491e',1,'VRDR::IJEMortality']]],
-  ['occupc4_20tt_20property_20tt_5',['OCCUPC4 &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1149',1,'']]],
-  ['of_20public_20methods_20of_20deathrecord_20and_20ijemortality_6',['Documentation of Public Methods of DeathRecord and IJEMortality',['../md__v_r_d_r_2_death_record.html#autotoc_md161',1,'']]],
+  ['occupc4_20tt_20property_20tt_5',['OCCUPC4 &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1152',1,'']]],
+  ['of_20public_20methods_20of_20deathrecord_20and_20ijemortality_6',['Documentation of Public Methods of DeathRecord and IJEMortality',['../md__v_r_d_r_2_death_record.html#autotoc_md164',1,'']]],
   ['oldeduc_7',['OLDEDUC',['../class_v_r_d_r_1_1_i_j_e_mortality.html#adc4c40d5e9c9678491da1be4d00b167c',1,'VRDR::IJEMortality']]],
-  ['oldeduc_20tt_20property_20tt_8',['OLDEDUC &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1151',1,'']]],
+  ['oldeduc_20tt_20property_20tt_8',['OLDEDUC &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1154',1,'']]],
   ['operator_20bundle_9',['operator Bundle',['../class_v_r_d_r_1_1_base_message.html#a77ebf929dc7957b03164dd6eb1a1843c',1,'VRDR::BaseMessage']]],
-  ['or_20time_20is_20explicitly_20unknown_10',['Specifying that a date or time is explicitly unknown',['../index.html#autotoc_md137',1,'']]],
-  ['organization_11',['Project Organization',['../index.html#autotoc_md132',1,'']]],
-  ['other_12',['other',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md54',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md51',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md47',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md35',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md44',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md40',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md37',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md32',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md29',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md27',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md24',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md21',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md19',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md14',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md11',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md4',1,'Other']]],
+  ['or_20time_20is_20explicitly_20unknown_10',['Specifying that a date or time is explicitly unknown',['../index.html#autotoc_md139',1,'']]],
+  ['organization_11',['Project Organization',['../index.html#autotoc_md134',1,'']]],
+  ['other_12',['other',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md56',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md53',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md49',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md37',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md46',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md42',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md39',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md34',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md31',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md29',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md26',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md23',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md21',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md16',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md13',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md6',1,'Other']]],
   ['othercondition_13',['OTHERCONDITION',['../class_v_r_d_r_1_1_i_j_e_mortality.html#af86ef038dffd9d91bed252753945825a',1,'VRDR::IJEMortality']]],
-  ['othercondition_20tt_20property_20tt_14',['OTHERCONDITION &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1153',1,'']]]
+  ['othercondition_20tt_20property_20tt_14',['OTHERCONDITION &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1156',1,'']]]
 ];
--- search/all_1d.js
+++ search/all_1d.js
@@ -1,18 +1,18 @@
 var searchData=
 [
   ['unitnum_5fr_0',['UNITNUM_R',['../class_v_r_d_r_1_1_i_j_e_mortality.html#a7b32ed8014c847343cbdc7abcc037c2f',1,'VRDR::IJEMortality']]],
-  ['unitnum_5fr_20tt_20property_20tt_1',['UNITNUM_R &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1367',1,'']]],
-  ['unknown_2',['Specifying that a date or time is explicitly unknown',['../index.html#autotoc_md137',1,'']]],
+  ['unitnum_5fr_20tt_20property_20tt_1',['UNITNUM_R &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1370',1,'']]],
+  ['unknown_2',['Specifying that a date or time is explicitly unknown',['../index.html#autotoc_md139',1,'']]],
   ['updategivenhumanname_3',['updateGivenHumanName',['../class_v_r_d_r_1_1_death_record.html#aec87893df7ca5bfb6614c48a1b3523f3',1,'VRDR::DeathRecord']]],
   ['updatemessagebundlerecord_4',['UpdateMessageBundleRecord',['../class_v_r_d_r_1_1_base_message.html#a5d3bdc84ad30f9df47d2251519909503',1,'VRDR::BaseMessage']]],
   ['url_5',['url',['../class_v_r_d_r_1_1_credentials.html#a07f27b98ca5dd504df7c98cfdad1778b',1,'VRDR.Credentials.Url'],['../class_v_r_d_r_1_1_client.html#ab758a1de8fd273be79a417b415497b2a',1,'VRDR.Client.Url']]],
-  ['usage_6',['usage',['../index.html#autotoc_md152',1,'Example Usage'],['../index.html#autotoc_md131',1,'Library Usage'],['../index.html#autotoc_md134',1,'Usage'],['../index.html#autotoc_md143',1,'Usage'],['../index.html#autotoc_md146',1,'Usage']]],
-  ['usages_7',['Example Usages',['../index.html#autotoc_md150',1,'']]],
+  ['usage_6',['usage',['../index.html#autotoc_md154',1,'Example Usage'],['../index.html#autotoc_md133',1,'Library Usage'],['../index.html#autotoc_md136',1,'Usage'],['../index.html#autotoc_md145',1,'Usage'],['../index.html#autotoc_md148',1,'Usage']]],
+  ['usages_7',['Example Usages',['../index.html#autotoc_md152',1,'']]],
   ['username_8',['Username',['../class_v_r_d_r_1_1_credentials.html#a58b540af324a81f572f32930eb532411',1,'VRDR::Credentials']]],
   ['using_20the_20vrdr_20filter_20library_9',['Using the VRDR.Filter Library',['../md_doc_2_filtering.html',1,'']]],
   ['using_20the_20vrdr_20messaging_20library_10',['Using the VRDR.Messaging Library',['../md_doc_2_messaging.html',1,'']]],
   ['usualindustry_11',['UsualIndustry',['../class_v_r_d_r_1_1_death_record.html#ad1052a091fd8367d65cf3176b0caa4a8',1,'VRDR::DeathRecord']]],
-  ['usualindustry_20tt_20property_20tt_12',['UsualIndustry &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md795',1,'']]],
+  ['usualindustry_20tt_20property_20tt_12',['UsualIndustry &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md798',1,'']]],
   ['usualoccupation_13',['UsualOccupation',['../class_v_r_d_r_1_1_death_record.html#a0721d602d9431aada87d08b8e54a7072',1,'VRDR::DeathRecord']]],
-  ['usualoccupation_20tt_20property_20tt_14',['UsualOccupation &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md798',1,'']]]
+  ['usualoccupation_20tt_20property_20tt_14',['UsualOccupation &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md801',1,'']]]
 ];

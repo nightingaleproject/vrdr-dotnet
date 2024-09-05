3c98f6f662afab566e7ddebcd57cd386629c035a
whichwit <whichwit@users.noreply.github.com>
2024-09-05 20:42:41 +0000

    deploy: 0c08541f6e9325f2bf772ff71b4525e044e57e83

--- search/all_18.js
+++ search/all_18.js
@@ -1,18 +1,18 @@
 var searchData=
 [
   ['occup_0',['OCCUP',['../class_v_r_d_r_1_1_i_j_e_mortality.html#a0c3b697af3301bac0d74f9382eabf8f2',1,'VRDR::IJEMortality']]],
-  ['occup_20tt_20property_20tt_1',['OCCUP &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1152',1,'']]],
+  ['occup_20tt_20property_20tt_1',['OCCUP &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1154',1,'']]],
   ['occupc_2',['OCCUPC',['../class_v_r_d_r_1_1_i_j_e_mortality.html#a7e652f58c9007bf7b9e329987302cc7b',1,'VRDR::IJEMortality']]],
-  ['occupc_20tt_20property_20tt_3',['OCCUPC &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1154',1,'']]],
+  ['occupc_20tt_20property_20tt_3',['OCCUPC &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1156',1,'']]],
   ['occupc4_4',['OCCUPC4',['../class_v_r_d_r_1_1_i_j_e_mortality.html#ab7ecbfa1ac98d380cfb0881cf7ce491e',1,'VRDR::IJEMortality']]],
-  ['occupc4_20tt_20property_20tt_5',['OCCUPC4 &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1156',1,'']]],
-  ['of_20public_20methods_20of_20deathrecord_20and_20ijemortality_6',['Documentation of Public Methods of DeathRecord and IJEMortality',['../md__v_r_d_r_2_death_record.html#autotoc_md168',1,'']]],
+  ['occupc4_20tt_20property_20tt_5',['OCCUPC4 &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1158',1,'']]],
+  ['of_20public_20methods_20of_20deathrecord_20and_20ijemortality_6',['Documentation of Public Methods of DeathRecord and IJEMortality',['../md__v_r_d_r_2_death_record.html#autotoc_md170',1,'']]],
   ['oldeduc_7',['OLDEDUC',['../class_v_r_d_r_1_1_i_j_e_mortality.html#adc4c40d5e9c9678491da1be4d00b167c',1,'VRDR::IJEMortality']]],
-  ['oldeduc_20tt_20property_20tt_8',['OLDEDUC &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1158',1,'']]],
+  ['oldeduc_20tt_20property_20tt_8',['OLDEDUC &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1160',1,'']]],
   ['operator_20bundle_9',['operator Bundle',['../class_v_r_d_r_1_1_base_message.html#a77ebf929dc7957b03164dd6eb1a1843c',1,'VRDR::BaseMessage']]],
-  ['or_20time_20is_20explicitly_20unknown_10',['Specifying that a date or time is explicitly unknown',['../index.html#autotoc_md143',1,'']]],
-  ['organization_11',['Project Organization',['../index.html#autotoc_md138',1,'']]],
-  ['other_12',['other',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md38',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md60',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md57',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md53',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md50',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md46',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md43',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md41',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md35',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md33',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md30',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md27',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md25',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md20',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md17',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md10',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md4',1,'Other']]],
+  ['or_20time_20is_20explicitly_20unknown_10',['Specifying that a date or time is explicitly unknown',['../index.html#autotoc_md145',1,'']]],
+  ['organization_11',['Project Organization',['../index.html#autotoc_md140',1,'']]],
+  ['other_12',['other',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md40',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md62',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md59',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md55',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md52',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md48',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md45',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md43',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md37',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md35',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md32',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md29',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md27',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md22',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md19',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md12',1,'Other'],['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md6',1,'Other']]],
   ['othercondition_13',['OTHERCONDITION',['../class_v_r_d_r_1_1_i_j_e_mortality.html#af86ef038dffd9d91bed252753945825a',1,'VRDR::IJEMortality']]],
-  ['othercondition_20tt_20property_20tt_14',['OTHERCONDITION &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1160',1,'']]]
+  ['othercondition_20tt_20property_20tt_14',['OTHERCONDITION &lt;tt&gt;property&lt;/tt&gt;',['../md__v_r_d_r_2_death_record.html#autotoc_md1162',1,'']]]
 ];
--- search/all_5.js
+++ search/all_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['5_204_200_205_20a_202023_206_2026_0',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.5&quot; &gt;4.0.5&lt;/a&gt; (2023-6-26)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md63',1,'']]],
-  ['5_204_201_205_20a_202023_209_205_1',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.1.5&quot; &gt;4.1.5&lt;/a&gt; (2023-9-5)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md34',1,'']]]
+  ['5_204_200_205_20a_202023_206_2026_0',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.0.5&quot; &gt;4.0.5&lt;/a&gt; (2023-6-26)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md65',1,'']]],
+  ['5_204_201_205_20a_202023_209_205_1',['&lt;a href=&quot;https://www.github.com/nightingaleproject/vrdr-dotnet/releases/tag/v4.1.5&quot; &gt;4.1.5&lt;/a&gt; (2023-9-5)',['../md__c_h_a_n_g_e_l_o_g.html#autotoc_md36',1,'']]]
 ];

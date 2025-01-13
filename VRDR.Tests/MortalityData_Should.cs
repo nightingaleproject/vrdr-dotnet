bf396d356c1956d8e1bf172d417506783caab047
Le Yang <le.yang@gmail.com>
2025-01-13 18:43:15 +0000

    add test case

--- VRDR.Tests/MortalityData_Should.cs
+++ VRDR.Tests/MortalityData_Should.cs
@@ -688,6 +688,378 @@ namespace VRDR.Tests
         }
 
         [Fact]
+        public void TestDeathRecordMortalityTranslation()
+        {
+            DeathRecord deathRecord = new DeathRecord();
+
+            // Identifier
+            deathRecord.Identifier = "1";
+
+            // CertifiedTime
+            deathRecord.CertifiedTime = "2019-01-29T16:48:06-05:00";
+
+            // RegisteredTime
+            deathRecord.RegisteredTime = "2019-02-01T16:47:04-05:00";
+
+            // CertificationRole
+            Dictionary<string, string> certificationRole = new Dictionary<string, string>();
+            certificationRole.Add("code", "434641000124105");
+            certificationRole.Add("system", "http://snomed.info/sct");
+            certificationRole.Add("display", "Physician");
+            deathRecord.CertificationRole = certificationRole;
+
+            // State Local Identifier
+            deathRecord.StateLocalIdentifier1 = "000000000042";
+
+            // MannerOfDeathType
+            Dictionary<string, string> mannerOfDeathType = new Dictionary<string, string>();
+            mannerOfDeathType.Add("code", "7878000");
+            mannerOfDeathType.Add("system", "http://snomed.info/sct");
+            mannerOfDeathType.Add("display", "Accidental death");
+            deathRecord.MannerOfDeathType = mannerOfDeathType;
+
+            // CertifierGivenNames
+            string[] cnames = { "Doctor", "Middle" };
+            deathRecord.CertifierGivenNames = cnames;
+
+            // CertifierFamilyName
+            deathRecord.CertifierFamilyName = "Last";
+
+            // CertifierSuffix
+            deathRecord.CertifierSuffix = "Jr.";
+
+            // CertifierAddress
+            Dictionary<string, string> caddress = new Dictionary<string, string>();
+            caddress.Add("addressLine1", "11 Example Street");
+            caddress.Add("addressLine2", "Line 2");
+            caddress.Add("addressCity", "Bedford");
+            caddress.Add("addressCounty", "Middlesex");
+            caddress.Add("addressState", "MA");
+            caddress.Add("addressZip", "01730");
+            caddress.Add("addressCountry", "US");
+            deathRecord.CertifierAddress = caddress;
+
+            Dictionary<string, string> certifierIdentifier = new Dictionary<string, string>();
+            certifierIdentifier.Add("system", "http://hl7.org/fhir/sid/us-npi");
+            certifierIdentifier.Add("value", "1234567890");
+            deathRecord.CertifierIdentifier = certifierIdentifier;
+
+            // // CertifierLicenseNumber
+            // deathRecord.CertifierLicenseNumber = "789123456";
+
+            // ContributingConditions
+            deathRecord.ContributingConditions = "Example Contributing Conditions";
+
+            // COD1A
+            deathRecord.COD1A = "Rupture of myocardium";
+
+            // INTERVAL1A
+            deathRecord.INTERVAL1A = "minutes";
+
+            // // CODE1A
+            // Dictionary<string, string> code1a = new Dictionary<string, string>();
+            // code1a.Add("code", "I21.0");
+            // code1a.Add("system", "http://hl7.org/fhir/sid/icd-10");
+            // code1a.Add("display", "Acute transmural myocardial infarction of anterior wall");
+            // deathRecord.CODE1A = code1a;
+
+            // COD1B
+            deathRecord.COD1B = "Acute myocardial infarction";
+
+            // INTERVAL1B
+            deathRecord.INTERVAL1B = "6 days";
+
+            // // CODE1B
+            // Dictionary<string, string> code1b = new Dictionary<string, string>();
+            // code1b.Add("code", "I21.9");
+            // code1b.Add("system", "http://hl7.org/fhir/sid/icd-10");
+            // code1b.Add("display", "Acute myocardial infarction, unspecified");
+            // deathRecord.CODE1B = code1b;
+
+            // COD1C
+            deathRecord.COD1C = "Coronary artery thrombosis";
+
+            // INTERVAL1C
+            deathRecord.INTERVAL1C = "5 years";
+
+            // COD1D
+            deathRecord.COD1D = "Atherosclerotic coronary artery disease";
+
+            // INTERVAL1D
+            deathRecord.INTERVAL1D = "7 years";
+
+            // GivenNames
+            deathRecord.GivenNames = new string[] { "Example", "Something", "Middle" };
+
+            // FamilyName
+            deathRecord.FamilyName = "Last";
+
+            // Suffix
+            deathRecord.Suffix = "Jr.";
+
+            // Gender
+            deathRecord.SexAtDeathHelper = ValueSets.AdministrativeGender.Male;
+
+            // BirthSex
+            //deathRecord.BirthSex = "F";
+
+            // DateOfBirth
+            deathRecord.DateOfBirth = "1940-02-19";
+
+            // Residence
+            Dictionary<string, string> raddress = new Dictionary<string, string>();
+            raddress.Add("addressLine1", "101 Example Street");
+            raddress.Add("addressLine2", "Line 2");
+            raddress.Add("addressCity", "Bedford");
+            raddress.Add("addressCounty", "Middlesex");
+            raddress.Add("addressState", "MA");
+            raddress.Add("addressZip", "01730");
+            raddress.Add("addressCountry", "US");
+            deathRecord.Residence = raddress;
+
+            // ResidenceWithinCityLimits
+            deathRecord.ResidenceWithinCityLimitsHelper = ValueSets.YesNoUnknown.No;
+
+            // SSN
+            deathRecord.SSN = "123456789";
+
+            // Ethnicity
+            deathRecord.Ethnicity2Helper = ValueSets.HispanicNoUnknown.Yes;
+
+            // Race
+            Tuple<string, string>[] race = { Tuple.Create(NvssRace.White, "Y"), Tuple.Create(NvssRace.NativeHawaiian, "Y"), Tuple.Create(NvssRace.OtherPacificIslander, "Y") };
+            deathRecord.Race = race;
+
+            // MaritalStatus
+            Dictionary<string, string> mscode = new Dictionary<string, string>();
+            mscode.Add("code", "S");
+            mscode.Add("system", "http://terminology.hl7.org/CodeSystem/v3-MaritalStatus");
+            mscode.Add("display", "Never Married");
+            deathRecord.MaritalStatus = mscode;
+
+            // FatherGivenNames
+            string[] fnames = { "Father", "Middle" };
+            deathRecord.FatherGivenNames = fnames;
+
+            // FatherFamilyName
+            deathRecord.FatherFamilyName = "Last";
+
+            // FatherSuffix
+            deathRecord.FatherSuffix = "Sr.";
+
+            // MotherGivenNames
+            string[] mnames = { "Mother", "Middle" };
+            deathRecord.MotherGivenNames = mnames;
+
+            // MotherMaidenName
+            deathRecord.MotherMaidenName = "Maiden";
+
+            // MotherSuffix
+            deathRecord.MotherSuffix = "Dr.";
+
+            // SpouseGivenNames
+            string[] spnames = { "Spouse", "Middle" };
+            deathRecord.SpouseGivenNames = spnames;
+
+            // SpouseFamilyName
+            deathRecord.SpouseFamilyName = "Last";
+
+            // SpouseSuffix
+            deathRecord.SpouseSuffix = "Ph.D.";
+
+            // BirthRecordId
+            deathRecord.BirthRecordId = "4242123";
+
+            // BirthRecordState
+            deathRecord.BirthRecordState = "MA";
+
+            // BirthRecordYear
+            deathRecord.BirthRecordYear = "1940";
+
+            // UsualOccupation
+            deathRecord.UsualOccupation = "secretary";
+
+            // UsualIndustry
+            deathRecord.UsualIndustry = "State agency";
+
+            // MilitaryService
+            Dictionary<string, string> mserv = new Dictionary<string, string>();
+            mserv.Add("code", "Y");
+            mserv.Add("system", "http://terminology.hl7.org/CodeSystem/v2-0136");
+            mserv.Add("display", "Yes");
+            deathRecord.MilitaryService = mserv;
+
+            // // MorticianGivenNames
+            // string[] fdnames = { "FD", "Middle" };
+            // deathRecord.MorticianGivenNames = fdnames;
+
+            // // MorticianFamilyName
+            // deathRecord.MorticianFamilyName = "Last";
+
+            // // MorticianSuffix
+            // deathRecord.MorticianSuffix = "Jr.";
+
+            // // MorticianIdentifier
+            // var mortId = new Dictionary<string, string>();
+            // mortId["value"] = "9876543210";
+            // mortId["system"] = "http://hl7.org/fhir/sid/us-npi";
+            // deathRecord.MorticianIdentifier = mortId;
+
+            // FuneralHomeAddress
+            Dictionary<string, string> fdaddress = new Dictionary<string, string>();
+            fdaddress.Add("addressLine1", "1011010 Example Street");
+            fdaddress.Add("addressLine2", "Line 2");
+            fdaddress.Add("addressCity", "Bedford");
+            fdaddress.Add("addressCounty", "Middlesex");
+            fdaddress.Add("addressState", "MA");
+            fdaddress.Add("addressZip", "01730");
+            fdaddress.Add("addressCountry", "US");
+            deathRecord.FuneralHomeAddress = fdaddress;
+
+            // FuneralHomeName
+            deathRecord.FuneralHomeName = "Smith Funeral Home";
+
+            // FuneralDirectorPhone
+            //deathRecord.FuneralDirectorPhone = "000-000-0000";
+
+            // DispositionLocationAddress
+            Dictionary<string, string> dladdress = new Dictionary<string, string>();
+            dladdress.Add("addressLine1", "603 Example Street");
+            dladdress.Add("addressLine2", "Line 2");
+            dladdress.Add("addressCity", "Bedford");
+            dladdress.Add("addressCounty", "Middlesex");
+            dladdress.Add("addressState", "MA");
+            dladdress.Add("addressZip", "01730");
+            dladdress.Add("addressCountry", "US");
+            deathRecord.DispositionLocationAddress = dladdress;
+
+            // DispositionLocationName
+            deathRecord.DispositionLocationName = "Bedford Cemetery";
+
+            // DecedentDispositionMethod
+            Dictionary<string, string> ddm = new Dictionary<string, string>();
+            ddm.Add("code", "449971000124106");
+            ddm.Add("system", "http://snomed.info/sct");
+            ddm.Add("display", "Burial");
+            deathRecord.DecedentDispositionMethod = ddm;
+
+            // AutopsyPerformedIndicator
+            Dictionary<string, string> api = new Dictionary<string, string>();
+            api.Add("code", "Y");
+            api.Add("system", "http://terminology.hl7.org/CodeSystem/v2-0136");
+            api.Add("display", "Yes");
+            deathRecord.AutopsyPerformedIndicator = api;
+
+            // AutopsyResultsAvailable
+            Dictionary<string, string> ara = new Dictionary<string, string>();
+            ara.Add("code", "Y");
+            ara.Add("system", "http://terminology.hl7.org/CodeSystem/v2-0136");
+            ara.Add("display", "Yes");
+            deathRecord.AutopsyResultsAvailable = ara;
+
+            // PregnancyStatus
+            Dictionary<string, string> ps = new Dictionary<string, string>();
+            ps.Add("code", "NA");
+            ps.Add("system", "http://terminology.hl7.org/CodeSystem/v3-NullFlavor");
+            ps.Add("display", "not applicable");
+            deathRecord.PregnancyStatus = ps;
+
+            // TransportationRole
+            Dictionary<string, string> tr = new Dictionary<string, string>();
+            tr.Add("code", "257500003");
+            tr.Add("system", "http://snomed.info/sct");
+            tr.Add("display", "Passenger");
+            deathRecord.TransportationRole = tr;
+
+            // ExaminerContacted
+            deathRecord.ExaminerContactedHelper = "N";
+
+            // TobaccoUse
+            Dictionary<string, string> tbu = new Dictionary<string, string>();
+            tbu.Add("code", "373066001");
+            tbu.Add("system", "http://snomed.info/sct");
+            tbu.Add("display", "Yes");
+            deathRecord.TobaccoUse = tbu;
+
+            // InjuryLocationAddress
+            Dictionary<string, string> iladdress = new Dictionary<string, string>();
+            iladdress.Add("addressLine1", "781 Example Street");
+            iladdress.Add("addressLine2", "Line 2");
+            iladdress.Add("addressCity", "Bedford");
+            iladdress.Add("addressCounty", "Middlesex");
+            iladdress.Add("addressState", "MA");
+            iladdress.Add("addressZip", "01730");
+            iladdress.Add("addressCountry", "US");
+            deathRecord.InjuryLocationAddress = iladdress;
+
+            // InjuryLocationName
+            deathRecord.InjuryLocationName = "Example Injury Location Name";
+
+            // InjuryDescription
+            deathRecord.InjuryDescription = "Example Injury Description";
+
+            // InjuryLocationDescription
+            //deathRecord.InjuryLocationDescription = "Example Injury Location Description";
+
+            // InjuryDate
+            deathRecord.InjuryDate = "2018-02-19T16:48:06-05:00";
+
+            // InjuryAtWork
+            Dictionary<string, string> codeIW = new Dictionary<string, string>();
+            codeIW.Add("code", "N");
+            codeIW.Add("system", "http://terminology.hl7.org/CodeSystem/v2-0136");
+            codeIW.Add("display", "No");
+            deathRecord.InjuryAtWork = codeIW;
+
+            // InjuryPlace
+            deathRecord.InjuryPlaceDescription = "Home";
+
+            // DeathLocationAddress
+            Dictionary<string, string> dtladdress = new Dictionary<string, string>();
+            dtladdress.Add("addressLine1", "671 Example Street");
+            dtladdress.Add("addressLine2", "Line 2");
+            dtladdress.Add("addressCity", "Bedford");
+            dtladdress.Add("addressCounty", "Middlesex");
+            dtladdress.Add("addressState", "MA");
+            dtladdress.Add("addressZip", "01730");
+            dtladdress.Add("addressCountry", "US");
+            deathRecord.DeathLocationAddress = dtladdress;
+
+            // DeathLocationName
+            deathRecord.DeathLocationName = "Example Death Location Name";
+
+            // DeathLocationDescription
+            deathRecord.DeathLocationDescription = "Example Death Location Description";
+
+            // DeathLocationType
+            Dictionary<string, string> deathLocationCode = new Dictionary<string, string>();
+            deathLocationCode.Add("code", "16983000");
+            deathLocationCode.Add("system", "http://snomed.info/sct");
+            deathLocationCode.Add("display", "Death in hospital");
+            deathRecord.DeathLocationType = deathLocationCode;
+
+            // DeathLocationJurisdiction
+            deathRecord.DeathLocationJurisdiction = "MA";
+
+            // DateOfDeath
+            deathRecord.DateOfDeath = "2018-02-19T16:48:06-05:00";
+
+            // AgeAtDeath
+            Dictionary<string, string> aad = new Dictionary<string, string>();
+            aad.Add("code", "a");
+            aad.Add("value", "79");
+            deathRecord.AgeAtDeath = aad;
+
+            // DateOfDeathPronouncement
+            deathRecord.DateOfDeathPronouncement = "2018-02-20T16:48:06-05:00";
+
+
+            IJEMortality ije = new IJEMortality(deathRecord);
+
+            Assert.Equal("Massachusetts", ije.DISPSTATE);
+        }
+
+        [Fact]
         public void TestTimeValues()
         {
             IJEMortality ijem;
